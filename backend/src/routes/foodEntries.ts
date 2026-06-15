732a9d048b23469e42cd5c3961ef0e5dc8ade576
yutakastep <127261743+yutakastep@users.noreply.github.com>
2026-06-15 13:26:50 -0400

    POST and GET food entries

--- backend/src/routes/foodEntries.ts
+++ backend/src/routes/foodEntries.ts
@@ -1,11 +1,82 @@
 import { Router } from "express";
+import { pool } from "../db.js";
 
 const router = Router();
 
-router.get("/", (_req, res) => {
-  res.json({
-    message: "food entries route working"
-  });
+// Implement POST food-entries
+
+router.post("/", async (req, res) => {
+  try {
+    const {
+      foodId,
+      servings,
+      entryDate,
+    } = req.body;
+
+    const userId = 1;
+
+    const result = await pool.query(
+      `
+      INSERT INTO food_entries
+      (
+        user_id,
+        food_id,
+        servings,
+        entry_date
+      )
+      VALUES ($1,$2,$3,$4)
+      RETURNING *
+      `,
+      [
+        userId,
+        foodId,
+        servings,
+        entryDate,
+      ]
+    );
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to create entry"
+    });
+  }
 });
 
-export default router; 
+
+// Implement GET food-entries
+
+router.get("/", async (_req, res) => {
+  try {
+    const result = await pool.query(
+      `
+      SELECT
+        fe.id,
+        f.name,
+        fe.servings,
+        fe.entry_date,
+        fe.created_at
+      FROM food_entries fe
+      JOIN foods f
+        ON fe.food_id = f.id
+      WHERE fe.user_id = $1
+      ORDER BY fe.entry_date DESC
+      `,
+      [1]
+    );
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to fetch entries",
+    });
+  }
+});
+
+
+export default router;
