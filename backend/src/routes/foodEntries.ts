caa6038c578b7a920a6d0e5213effe37ccba9ebc
yutakastep <127261743+yutakastep@users.noreply.github.com>
2026-06-15 13:27:45 -0400

    get food entries based on id

--- backend/src/routes/foodEntries.ts
+++ backend/src/routes/foodEntries.ts
@@ -78,5 +78,50 @@ router.get("/", async (_req, res) => {
   }
 });
 
+// GET food entries based on id
+// similar process as done with foods
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      return res.status(400).json({
+        error: "Invalid entry ID",
+      });
+    }
+
+    const result = await pool.query(
+      `
+      SELECT
+        fe.id,
+        f.name,
+        fe.servings,
+        fe.entry_date,
+        fe.created_at
+      FROM food_entries fe
+      JOIN foods f
+        ON fe.food_id = f.id
+      WHERE fe.id = $1
+        AND fe.user_id = $2
+      `,
+      [id, 1]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        error: "Food entry not found",
+      });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to fetch food entry",
+    });
+  }
+});
 
 export default router;
