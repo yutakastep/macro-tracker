ea811fdf1a97703580d51665dab5ef8816e7cace
yutakastep <127261743+yutakastep@users.noreply.github.com>
2026-06-23 17:28:55 -0400

    update App.tsx, create types.ts

---
+++ frontend/src/types.ts
@@ -0,0 +1,13 @@
+export interface DashboardData {
+  calories: string;
+  protein: string;
+  carbs: string;
+  fat: string;
+}
+
+export interface FoodEntry {
+  id: number;
+  name: string;
+  servings: string;
+  entry_date: string;
+}
