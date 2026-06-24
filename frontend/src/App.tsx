7d23904178dde7e57cae17ea4acfb6f0ca3d65eb
yutakastep <127261743+yutakastep@users.noreply.github.com>
2026-06-24 17:10:53 -0400

    add food entries to app frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,32 @@
 import { useEffect, useState } from "react";
-import type { DashboardData } from "./types";
+import type { 
+  DashboardData, 
+  FoodEntry, 
+} from "./types";
 
+// start react, then:
 function App() {
   const [dashboard, setDashboard] =
     useState<DashboardData | null>(null);
+  
+  const [entries, setEntries] =
+    useState<FoodEntry[]>([]);
 
+  // send request for dashboard
   useEffect(() => {
     fetch("http://localhost:5000/dashboard/today")
       .then((res) => res.json())
       .then((data) => setDashboard(data));
   }, []);
+  // backend receives GET, responses come back as JSON
+
+  //send request for food entries
+  useEffect(() => {
+    fetch("https://localhost:5000/food-entries")
+      .then((res) => res.json())
+      .then((data) => setEntries(data));
+  }, []);
+  // backend receives GET, responses come back as JSON
 
   return (
     <div>
@@ -23,6 +40,15 @@ function App() {
           <p>Fat: {dashboard.fat}</p>
         </>
       )}
+
+      <h2>Today's Entries</h2>
+      
+      {entries.map((entry) => (
+        <div key={entry.id}>
+          <p>{entry.name}</p>
+          <p>{entry.servings}</p>
+        </div>
+      ))}
     </div>
   );
 }
